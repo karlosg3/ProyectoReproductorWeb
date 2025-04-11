24651af2119024943c8b356dfca441d82db4c691
Nordico18 <andrew413300@gmail.com>
2025-04-11 08:26:06 -0700

    feat: edicion de texto

--- reproductorwebteriyaki/src/App.js
+++ reproductorwebteriyaki/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Editado por Apaez, oli :)
+          mijo donde van, oli :)
         </p>
       </header>
     </div>
