21bf65d3538facbd47b4287a3cd23fe321f8f34f
K4 <llamandoakalos@gmail.com>
2025-05-18 13:52:52 -0700

    feat: Añade ruteo

--- reproductorwebteriyaki/src/screens/home.js
+++ reproductorwebteriyaki/src/screens/home.js
@@ -1,8 +1,37 @@
 import Form, { FormLabel } from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from 'react';
+import UserScreen from './User';
+import PlaylistScreen from './Playlist';
+import AlbumScreen from './Album';
+import ArtistaScreen from './Artista';
+import BibliotecaScreen from './Biblioteca';
+import CancionScreen from './Cancion';
+import FavoritasScreen from './Favoritas';
+import HistorialScreen from './Historial';
+import LogInScreen from './LogIn';
+import LyricsScreen from './Lyrics';
+import RegistroScreen from './Registro';
 
 function HomeScreen(){
     return(
-        <FormLabel>Home</FormLabel>
+        <Router>
+            <Routes>
+                //Se utilizara un identiifcador para la ruta especifica de los artistas, albums y canciones
+                <Route path="/" element={<FormLabel>Home</FormLabel>} />
+                <Route path="/album" element={<AlbumScreen />} />
+                <Route path="/artista" element={<ArtistaScreen />} />
+                <Route path="/biblioteca" element={<BibliotecaScreen />} />
+                <Route path="/cancion" element={<CancionScreen />} />
+                <Route path="/biblioteca/favoritas" element={<FavoritasScreen />} />
+                <Route path="/biblioteca/historial" element={<HistorialScreen />} />
+                <Route path="/login" element={<LogInScreen />} />
+                <Route path="/lyrics" element={<LyricsScreen />} />
+                <Route path="/playlist" element={<PlaylistScreen />} />
+                <Route path="/registro" element={<RegistroScreen />} />
+                <Route path="/user" element={<UserScreen />} />
+            </Routes>
+        </Router>
     )
 }
 
