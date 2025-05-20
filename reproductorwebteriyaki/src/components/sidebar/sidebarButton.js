8942a1d4fd68d581aa21c4ca6753074f707f42ab
K4 <llamandoakalos@gmail.com>
2025-05-19 20:45:55 -0700

    refactor: Modifica animacion del Sidebar

--- reproductorwebteriyaki/src/components/sidebar/sidebarButton.js
+++ reproductorwebteriyaki/src/components/sidebar/sidebarButton.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { IconContext } from "react-icons";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { FormLabel } from "react-bootstrap";
 import "./sidebarButton.css";
 
 export default function SidebarButton(props){
+    const location = useLocation();
+    const isActive = location.pathname === props.to;
+    const btnClass = isActive ? "sidebar-button active" : "sidebar-button";
     return(
         <Link to={props.to}>
-            <div className="sidebar-button">
+            <div className={btnClass}>
                 <IconContext.Provider value={{size: '100%', className:"sidebar-Icon"}}>
                     {props.icon}
                     <FormLabel className="sidebar-text">{props.Text}</FormLabel>
