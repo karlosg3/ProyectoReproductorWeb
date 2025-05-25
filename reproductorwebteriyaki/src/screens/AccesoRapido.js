0f95bc6c924f951c2394457ef99c34096fb6f7be
K4 <llamandoakalos@gmail.com>
2025-05-24 22:38:09 -0700

    refactor: Inicia el agregado de tarjetas

--- reproductorwebteriyaki/src/screens/AccesoRapido.js
+++ reproductorwebteriyaki/src/screens/AccesoRapido.js
@@ -1,8 +1,21 @@
-import { FormLabel } from "react-bootstrap";
+import { Form, FormLabel } from "react-bootstrap";
+//import { Card, CardContent } from "@/components/ui/card"
 
 function AccesoRapidoScreen(){
+   // const AlbumCard = ({ title, artist, image}) => (
+//        <Card className="album-card">
+//            <CardContent>
+//                <img src={image} alt={title} className="album-image" />
+//                <FormLabel className="album-title">{title}</FormLabel>
+//                <FormLabel className="album-artist">{artist}</FormLabel>
+//            </CardContent>
+//        </Card>
+  //  )
+
     return(
-        <FormLabel>Acceso Rapido</FormLabel> 
+        <main className="acceso-rapido-container">
+            <FormLabel className="recomendacion-label">Para ti</FormLabel>
+        </main>
     )
 }
 
