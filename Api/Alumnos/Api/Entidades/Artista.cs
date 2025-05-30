e17f140135cd29e43c39699da52cc1acfd75e344
cesarsoto123 <120688503+cesarsoto123@users.noreply.github.com>
2025-05-29 23:13:23 -0700

    feat: Configuracion Controlador Artista

--- Api/Alumnos/Api/Entidades/Artista.cs
+++ Api/Alumnos/Api/Entidades/Artista.cs
@@ -4,13 +4,13 @@
     {
         public int Id { get; set; }
         public string Nombre { get; set; }
-        public string Imagen { get; set; }
-        public string Descripcion { get; set; }
+        public string? Imagen { get; set; }
+        public string? Descripcion { get; set; }
+        public string Slug { get; set; }
+        public bool Habilitado { get; set; }
 
-        public String Slug { get; set; }
-        public Boolean Habilitado { get; set; }
-
-        public ICollection<Album> Albums { get; set; }
-        public ICollection<Colaboracion> Colaboraciones { get; set; }
+        public ICollection<Album> Albums { get; set; } = new List<Album>();
+        public ICollection<Cancion> Canciones { get; set; } = new List<Cancion>();
+        public ICollection<Colaboracion> Colaboraciones { get; set; } = new List<Colaboracion>();
     }
 }
---
+++ Api/Alumnos/Api/Persistencia/Configuraciones/ArtistaConfiguracion.cs
@@ -0,0 +1,23 @@
+﻿using Api.Entidades;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Api.Persistencia.Configuraciones
+{
+    public class ArtistaConfiguracion : IEntityTypeConfiguration<Artista>
+    {
+        public void Configure(EntityTypeBuilder<Artista> builder)
+        {
+            builder.ToTable("Artistas");
+
+            builder.HasKey(a => a.Id);
+
+            builder.Property(a => a.Nombre).IsRequired().HasMaxLength(100);
+            builder.Property(a => a.Imagen).HasMaxLength(255);
+            builder.Property(a => a.Descripcion).HasMaxLength(500);
+            builder.Property(a => a.Slug).IsRequired().HasMaxLength(120);
+            builder.Property(a => a.Habilitado).HasDefaultValue(true);
+
+        }
+    }
+}
